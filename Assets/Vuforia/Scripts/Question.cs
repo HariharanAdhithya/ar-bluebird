9b12dc14e5e026de042b5c5a903da1b4baaf8ce4
Vinoth Vijayakumar <vinoth.ubergeek@gmail.com>
2018-11-23 17:59:05 -0700

    Question class

---
+++ Assets/Vuforia/Scripts/Question.cs
@@ -0,0 +1,41 @@
+﻿using System;
+
+namespace AssemblyCSharp
+{
+	public class QuestionA
+	{
+		public  String ques;
+		public List<String> choices;
+		public String ans;
+
+		public QuestionA ()
+		{
+		}
+
+		public QuestionA (string q, List<String> c, String a)
+		{
+			ques = q;
+			choices = c;
+			ans = a;
+		
+		}
+
+		public string Ques {
+			get {
+				return this.ques;
+			}
+		}
+
+		public List Choices {
+			get {
+				return this.choices;
+			}
+		}
+
+		public string Ans {
+			get {
+				return this.ans;
+			}
+		}
+	}
+
